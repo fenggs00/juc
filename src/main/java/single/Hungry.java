7e55b5160afcd9162a867d4fca933f4608edeebe
fgs <973294704@qq.com>
2021-09-29 19:34:48 +0800

    第一次修改

--- src/main/java/single/Hungry.java
+++ src/main/java/single/Hungry.java
@@ -3,11 +3,15 @@ package single;
 
 //饿汉式单例    --浪费资源 类加载的时候就加载了
 //1.单例模式私有化构造器
+
+//2.用静态常量 装对象//2.用静态常量 装对象
+
 //2.用静态常量 装对象
 //3.静态方法获取对象
 public class Hungry {
 
     private Hungry(){
+
 
     }
     private  final  static  Hungry HUNGRY = new Hungry();
